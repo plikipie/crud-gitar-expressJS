ea1159df506f47f56236a6175280f1f7899defa3
plikipe <plikipie@gmail.com>
2020-09-29 18:29:59 +0700

    SUCCES CREATE AND CONNECT TO DB

--- routes/gitar.js
+++ routes/gitar.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 const gitarcontroller = require("../controllers/gitarcontroller");
 
 router.get("/", gitarcontroller.viewGitar);
-router.get("/", gitarcontroller.addGitar);
+router.post("/", gitarcontroller.addGitar);
 
 module.exports = router;
