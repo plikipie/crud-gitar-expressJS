dd25c6c47096646d246469a78bacbba71e247639
plikipe <plikipie@gmail.com>
2020-09-30 18:50:20 +0700

    Menambahkan views body

--- models/gitar.js
+++ models/gitar.js
@@ -2,34 +2,34 @@ const mongoose = require("mongoose");
 
 // Membuat variable baru dengan nama gitarScheme
 const gitarScheme = new mongoose.Schema({
-    id_gitar: {
-        type: Number,
-        required: true,
-    },
-    merk: {
-        type: String,
-        required: true,
-    },
-    jeniskayu: {
-        type: String,
-        required: true,
-    },
-    fret: {
-        type: Number,
-        required: true,
-    },
-    seri: {
-        type: String,
-        required: true,
-    },
-    deskripsi: {
-        type: String,
-        required: true,
-    },
-    foto: {
-        type: String,
-        required: true,
-    },
+  id_gitar: {
+    type: String,
+    required: true,
+  },
+  merk: {
+    type: String,
+    required: true,
+  },
+  jeniskayu: {
+    type: String,
+    required: true,
+  },
+  fret: {
+    type: Number,
+    required: true,
+  },
+  seri: {
+    type: String,
+    required: true,
+  },
+  deskripsi: {
+    type: String,
+    required: true,
+  },
+  foto: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Gitar", gitarScheme);
